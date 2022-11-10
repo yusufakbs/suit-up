1b3531492bbbede97c47b3c10a89668f9c54a75a
ahmet <ahmetfuran1998@gmail.com>
2022-11-10 23:08:56 +0300

    3d module has been added

--- app/src/main/java/com/example/suit_up/MainActivity.java
+++ app/src/main/java/com/example/suit_up/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.suit_up;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.example.suit_up.UnityShowActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void open3DActivity (View view){
+        Intent intent = new Intent (this, UnityShowActivity.class);
+        startActivity(intent);
     }
 }
