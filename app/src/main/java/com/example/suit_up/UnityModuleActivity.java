a08072a96260a26921a86cfd8160d16fd215e7bc
Ahmet Yenipinar <ahmet.yenipinar@mera.com>
2022-11-22 13:44:09 +0300

    3D Unity Activity updated as to be a subview in activity

--- app/src/main/java/com/example/suit_up/UnityModuleActivity.java
+++ app/src/main/java/com/example/suit_up/UnityModuleActivity.java
@@ -1,20 +1,44 @@
 package com.example.suit_up;
 
+import com.google.android.material.navigation.NavigationView;
 import com.unity3d.player.UnityPlayerActivity;
+import com.unity3d.player.UnityPlayer;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import android.app.Activity;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
 import android.util.Log;
 
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout.LayoutParams;
 
-public class UnityModuleActivity extends UnityPlayerActivity {
+
+public class UnityModuleActivity extends Activity {
+    private UnityPlayer mUnityPlayer;
+
+//    private DrawerLayout mDrawer;
+//    private NavigationView nvDrawer;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Calls UnityPlayerActivity.onCreate()
+
         super.onCreate(savedInstanceState);
-        // Prints debug message to Logcat
-        Log.d("OverrideActivity", "onCreate called!");
-    }
-    public void onBackPressed()
-    {
-        // Instead of calling UnityPlayerActivity.onBackPressed(), this example ignores the back button event
-        // super.onBackPressed();
+        mUnityPlayer = new UnityPlayer(this);
+        int glesMode = mUnityPlayer.getSettings().getInt("gles_mode", 1);
+        boolean trueColor8888 = false;
+        mUnityPlayer.init(glesMode, trueColor8888);
+        //Set the content to main
+        setContentView(R.layout.unity_module_activity);
+
+        //Inflate the frame layout from XML
+        FrameLayout frameLayout = (FrameLayout)findViewById(R.id.unityFrameLayout);
+        LayoutParams layoutParameters = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT);
+        frameLayout.addView(mUnityPlayer.getView(), 0, layoutParameters);
+        mUnityPlayer.windowFocusChanged(true);
+        mUnityPlayer.resume();
     }
 }
--- unityModule/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
+++ unityModule/unityLibrary/src/main/Il2CppOutputProject/Source/il2cppOutput/lumpedcpp/Lump_libil2cpp_codegen.cpp
@@ -1,4 +1,4 @@
 #include "il2cpp-config.h"
-#include "D:\hackathon_2022\suit-up\unityModule\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
-#include "D:\hackathon_2022\suit-up\unityModule\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
-#include "D:\hackathon_2022\suit-up\unityModule\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
+#include "D:\hackathon_2022\clone3\suit-up\unityModule\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-common.cpp"
+#include "D:\hackathon_2022\clone3\suit-up\unityModule\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-il2cpp.cpp"
+#include "D:\hackathon_2022\clone3\suit-up\unityModule\unityLibrary\src\main\Il2CppOutputProject\IL2CPP\libil2cpp\codegen\il2cpp-codegen-tiny.cpp"
