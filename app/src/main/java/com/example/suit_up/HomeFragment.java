58a543a10b7d13c208ecb9dce5603d41ca5d7316
Ahmet Furkan <35719856+AhmetFurkann@users.noreply.github.com>
2022-12-02 13:59:48 +0300

    Clicking property added to the cardview of 3DUnityShowRoom (#13)

--- app/src/main/java/com/example/suit_up/HomeFragment.java
+++ app/src/main/java/com/example/suit_up/HomeFragment.java
@@ -8,14 +8,23 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.Nullable;
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 
 
 public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState){
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        CardView unityCardView = (CardView) view.findViewById(R.id.imageCard);
+        unityCardView.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent (HomeFragment.this.getActivity(), UnityModuleActivity.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
-
     public void open3DActivity(View view) {
         Intent intent = new Intent (HomeFragment.this.getActivity(), UnityModuleActivity.class);
         startActivity(intent);
