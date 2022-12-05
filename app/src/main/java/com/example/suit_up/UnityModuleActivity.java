302a177b31391b5b0f341cfeac1c1afc8b0b8e29
Ahmet Furkan <35719856+AhmetFurkann@users.noreply.github.com>
2022-12-06 02:58:44 +0300

    Wardrobe click feature added (#18)
    Co-authored-by: Ahmet Yenipinar <ahmet.yenipinar@mera.com>

--- app/src/main/java/com/example/suit_up/UnityModuleActivity.java
+++ app/src/main/java/com/example/suit_up/UnityModuleActivity.java
@@ -5,6 +5,7 @@ import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 import com.unity3d.player.UnityPlayer;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
@@ -13,8 +14,10 @@ import android.widget.FrameLayout;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
 import android.widget.PopupWindow;
+import android.widget.ScrollView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 
 public class UnityModuleActivity extends AppCompatActivity{
@@ -32,7 +35,6 @@ public class UnityModuleActivity extends AppCompatActivity{
 
         setContentView(R.layout.unity_module_activity);
 
-
         FrameLayout frameLayout = (FrameLayout) findViewById(R.id.unityFrameLayout);
         ExtendedFloatingActionButton e_fab = (ExtendedFloatingActionButton) findViewById(R.id.extended_fab);
         ExtendedFloatingActionButton fab = (ExtendedFloatingActionButton) findViewById(R.id.extended_size_button);
@@ -41,6 +43,16 @@ public class UnityModuleActivity extends AppCompatActivity{
                 openPopUpWardrobe(v);
             }
         });
+
+//        ScrollView wardrobeView = (ScrollView) findViewById(R.layout.wardrobe_pop_up_layout);
+//        CardView unityCardView = (CardView) war.findViewById(R.id.cloth_1_size_s);
+//        unityCardView.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view) {
+//                UnityPlayer.UnitySendMessage("ClothManager", "activate_cloth_1_size_s", "Test");
+//            }
+//        });
+
 
         ExtendedFloatingActionButton closer_view_fab = (ExtendedFloatingActionButton) findViewById(R.id.closer_view_fab);
 //        fab.setVisibility(View.GONE);
@@ -74,10 +86,16 @@ public class UnityModuleActivity extends AppCompatActivity{
         int height = LinearLayout.LayoutParams.WRAP_CONTENT;
         boolean focusable = true; // lets taps outside the popup also dismiss it
         final PopupWindow popupWindow = new PopupWindow(popupView, 850, 1500, focusable);
-
-        // show the popup window
-        // which view you pass in doesn't matter, it is only used for the window tolken
         popupWindow.showAtLocation(view, Gravity.CENTER, 0, 0);
+
+
+        popupWindow.getContentView().findViewById(R.id.cloth_1_size_s).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                UnityPlayer.UnitySendMessage("ClothManager", "activate_cloth_1_size_s", "Test");
+                popupWindow.dismiss();
+            }
+        });
     }
 
     public void switchClothes(View view) {
